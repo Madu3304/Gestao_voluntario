fbad8258d8c03092d599258e09a5306dfa1ddf72
LuizFelipeSchroderMarcon <lschrodermarcon@gmail.com>
2024-06-28 19:48:15 -0300

    criando rotas para cidade

--- script/controller/cidade_controller.js
+++ script/controller/cidade_controller.js
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import Cidade from "../models/cidade_model.js";
 
 const cidade = {}
@@ -8,6 +9,50 @@ cidade.getCidade = async(req,res)=>{
         res.send(cidades)
     } catch (e) {
         console.log("Erro ao tentar consultar as cidades.......", e)
+    }
+}
+
+cidade.createCidade = async(req,res)=>{
+    try {
+        const {ds_cidade, cd_estado} = req.body
+        const novaCidade = Cidade.create({
+            ds_cidade: ds_cidade,
+            cd_estado: cd_estado
+        })
+        res.send(novaCidade)
+    } catch (e) {
+        console.log("Erro ao tentar inserir uma nova cidade.....", e)
+    }
+}
+
+cidade.updateCidade = async(req,res)=>{
+    try {
+        const {cd_cidade} = req.params
+        const {ds_cidade, cd_estado} = req.body
+        
+        await Cidade.update(
+            {
+                ds_cidade:ds_cidade,
+                cd_estado:cd_estado
+            },
+            {where: {cd_cidade:cd_cidade}}
+        )
+        const cidadeAtualizada = await Cidade.findByPk(cd_cidade)
+        res.send(cidadeAtualizada)
+    } catch (e) {
+        console.log("Erro ao tentar atualizar uma cidade.....", e)
+    }
+}
+
+cidade.deleteCidade = async(req,res)=>{
+    try {
+        const {cd_cidade} = req.params
+        await Cidade.destroy({
+            where:{cd_cidade:cd_cidade}
+        })
+        res.send({message: 'Cidade deletada com sucesso'})
+    } catch (e) {
+        console.log("Erro ao tentar deletar cidade.....", e)
     }
 }
 
--- script/routes/cidade_routes.js
+++ script/routes/cidade_routes.js
@@ -4,5 +4,8 @@ import { cidade } from "../controller/cidade_controller.js";
 const router = express.Router()
 
 router.get('/cidade', cidade.getCidade)
+router.post('/cidade', cidade.createCidade)
+router.put('/cidade/:cd_cidade', cidade.updateCidade)
+router.delete('/cidade/:cd_cidade', cidade.deleteCidade)
 
 export {router}
