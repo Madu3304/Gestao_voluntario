3a778168b9ebb5f2bb57a2f31c8778ec7d565800
LuizFelipeSchroderMarcon <lschrodermarcon@gmail.com>
2024-06-30 23:33:32 -0300

    implementacao de token na tela de login

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ import { router as cidadeRoutes} from "./script/routes/cidade_routes.js";
 import { router as estadoRoutes } from "./script/routes/estado_routes.js";
 import { router as voluntarioRoutes } from "./script/routes/voluntario_routes.js";
 import { router as voluntarioEventoRoutes } from "./script/routes/voluntario_evento_routes.js";
+import { router as authRoutes } from "./script/routes/auth_routes.js";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+server.use(cors())
+server.use(bodyParser.json())
+
 
 server.use(express.static(path.join(__dirname, 'views')))
 
@@ -54,14 +61,12 @@ server.get('/voluntario', (req,res)=>{
 })
 
 
-server.use(express.json());
-server.use(express.urlencoded({extended:true}));
-
 server.use(eventoRoutes)
 server.use(cidadeRoutes)
 server.use(estadoRoutes)
 server.use(voluntarioRoutes)
 server.use(voluntarioEventoRoutes)
+server.use(authRoutes)
 
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
---
+++ script/routes/auth_routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+    const { user, pwd } = req.body;
+
+    if (user === 'camargo' && pwd === '123') {
+        const accessToken = jwt.sign({ username: user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+        res.json({ token: accessToken, redirectUrl: '/home'});
+    } else {
+        res.status(401).send('Credenciais inválidas');
+    }
+});
+
+router.post('/logout', function(req,res){
+    res.json({auth:false, token:null})
+})
+
+export { router };
