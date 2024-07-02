7943fc1da4d918dcdb30acdf057b0a26d7fcde6f
Madu3304 <Mduda0722@gmail.com>
2024-07-02 13:01:14 -0300

    ajuste- menu

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
 /* para funcionar*/
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
+
+function menuShow() {
+    let menuMobile = document.querySelector('#sidebar');
+    if (menuMobile.classList.contains('open-sidebar')) {
+        menuMobile.classList.remove('open-sidebar');
+        document.querySelector('#open_btn_icon').classList.remove('rotate');
+    } else {
+        menuMobile.classList.add('open-sidebar');
+        document.querySelector('#open_btn_icon').classList.add('rotate');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let logo = document.getElementById('user_logo');
+    logo.addEventListener('click', menuShow);
 });
