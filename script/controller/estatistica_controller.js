c7a71e2fed3ef58344a2454bda5942c5f09b257e
Isaac Graper <isaac.graper@gmail.com>
2024-07-03 13:13:18 -0300

    criado estatistica routes e update na controller

--- script/controller/estatistica_controller.js
+++ script/controller/estatistica_controller.js
@@ -1,15 +1,54 @@
-import Evento from "../models/evento_model.js"
-import Voluntario from "../models/voluntario.model.js";
-import Voluntario_Evento from "../models/voluntario_evento_model.js";
+import sequelize from './config/connection.js';
 
-let evento = {};
+let evento_contagem = {};
 
-evento.getCountEventos = async (req, res) => {
+evento_contagem.getCountEventos = async (req, res) => {
   try {
-    const [eventos] = await sequelize.query('SELECT COUNT(*) FROM Eventos');
-    res.send(eventos);
+    const [result] = await sequelize.query('SELECT COUNT(*) AS totalEventos FROM Eventos');
+    
+    if (!result || !result[0] || !result[0].totalEventos) {
+      throw new Error('Erro ao obter contagem de eventos.');
+    }
+
+    res.json({ totalEventos: result[0].totalEventos });
   } catch (e) {
-    console.error('Erro ao tentar pegar todos os eventos:', e);
-    res.status(500).send('Erro ao tentar pegar todos os eventos');
+    console.error('Erro ao contar eventos:', e);
+    res.status(500).send('Erro ao contar os eventos.');
   }
 };
+
+let voluntario_contagem = {};
+
+voluntario_contagem.getCountVoluntarios = async (req, res) => {
+  try {
+    const [result] = await sequelize.query('SELECT COUNT(*) AS totalVoluntarios FROM Voluntarios');
+
+    if (!result || !result[0] || !result[0].totalVoluntarios) {
+      throw new Error('Erro ao obter contagem de voluntários.');
+    }
+
+    res.json({ totalVoluntarios: result[0].totalVoluntarios });
+  } catch (e) {
+    console.error('Erro ao contar voluntários:', e);
+    res.status(500).send('Erro ao contar os voluntários.');
+  }
+};
+
+// let voluntario_ranking = {};
+
+// voluntario_ranking.getRankingVoluntario = async (req, res) => {
+//   try {
+//     const [result] = await sequelize.query('SELECT * FROM Voluntarios ORDER BY ? DESC LIMIT 3');
+
+//     if (!result) {
+//       throw new Error('Nenhum resultado encontrado para o ranking de voluntários.');
+//     }
+
+//     res.json(result);
+//   } catch (e) {
+//     console.error('Erro ao listar o ranking de voluntários:', e);
+//     res.status(500).send('Erro ao listar o ranking de voluntários.');
+//   }
+// };
+
+export { voluntario_contagem, evento_contagem, /* voluntario_ranking */ };
